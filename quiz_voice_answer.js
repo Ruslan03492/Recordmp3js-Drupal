a37b2c7196f7d63657a9063dde5b5d6f4b455275
Ruslan Tsialiak <ruslan_03492@2804867.no-reply.drupal.org>
2015-06-01 12:32:48 +0300

    Initial commit

---
+++ quiz_voice_answer.js
@@ -0,0 +1,76 @@
+function __log(e, data) {
+  log.innerHTML = "\n" + e + " " + (data || '');
+}
+
+var audio_context;
+var recorder;
+var audioStream = null;
+
+var rec = document.getElementById('rec');
+if (rec) {
+  rec.addEventListener(
+    'click', function () {
+      if (navigator.getUserMedia) {
+        navigator.getUserMedia(
+          {audio: true}, startUserMedia, function (e) {
+            __log('No live audio input: ' + e);
+          }
+        );
+      }
+    }
+  );
+}
+
+function startUserMedia(stream) {
+  audioStream = stream;
+  var input = audio_context.createMediaStreamSource(stream);
+  input.connect(audio_context.destination);
+  recorder = new Recorder(input);
+  startRecording(rec);
+}
+
+function startRecording(button) {
+  recorder && recorder.record();
+  button.disabled = true;
+  button.nextElementSibling.disabled = false;
+  __log('Recording...');
+}
+
+function stopRecording(button) {
+  recorder && recorder.stop();
+  button.disabled = true;
+  button.previousElementSibling.disabled = false;
+  // create WAV download link using audio data blob.
+  createDownloadLink();
+  recorder.clear();
+}
+
+function createDownloadLink() {
+  recorder && recorder.exportWAV(function (blob) {
+  });
+}
+
+var stop = document.getElementById('stop');
+if (stop) {
+  stop.addEventListener(
+    'click', function () {
+      stopRecording(this);
+      audioStream.stop();
+    }
+  );
+}
+
+window.onload = function init() {
+  try {
+    // webkit shim.
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    navigator.getUserMedia = ( navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia);
+    window.URL = window.URL || window.webkitURL;
+    audio_context = new AudioContext;
+  } catch (e) {
+    alert('No web audio support in this browser!');
+  }
+};
